ce433c885ae25512a44cdc346acb2c9776905c43
Dominic Shaw <dominicshaw@hotmail.com>
2016-09-01 13:19:14 +0100

    ...

--- ShipmentWeb/app/config.js
+++ ShipmentWeb/app/config.js
@@ -1,3 +1,3 @@
-﻿var AUTH0_DOMAIN = 'ttint.eu.auth0.com';
-var AUTH0_CLIENT_ID = '2ysmY2WcQrwaDY1Jg8woaNcFNR6eQfoA';
+﻿var AUTH0_DOMAIN = 'xxx.eu.auth0.com';
+var AUTH0_CLIENT_ID = 'xxx';
 var SERVICE_BASE = 'http://localhost:62843';
